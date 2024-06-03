10d7161168b7b96512db6af52bea0f2e789af2da
Rigoberto09 <rigobertoborjas3@gmail.com>
2024-06-02 21:51:04 -0600

    Incorporacion de prisma

--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductoService } from './producto.service';
 import { ProductoController } from './producto.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [ProductoService],
-  controllers: [ProductoController]
+  controllers: [ProductoController],
+  imports:[PrismaModule]
 })
 export class ProductoModule {}
